74960e334b72406b6f726ef80b340c7e15e09547
s-a-c-1-8 <Sac@8teen.in>
2023-06-14 18:46:49 +0530

    responsive

--- public/js/main.js
+++ public/js/main.js
@@ -17,46 +17,65 @@ hideButtons = () => {
   button5.style.display = "none"
   img.style.display = 'none'
 }
+
+// On Video End
+myFunction = () =>  {
+  button2.style.display = 'block'
+  button4.style.display = "block"
+  if(counter > 1){
+    button3.style.display = "block"
+  }
+  changeButton()
+}
 changeButton = () => {
+  button4.className = "animate__animated animate__pulse animate__infinite	infinite"
+  button4.style.paddingTop = ''
+  button4.style.paddingBottom = ''
+  button4.style.paddingLeft = ''
+  button4.style.top =""
+  button4.style.right =""
+  button4.style.left =""
+  button4.style.bottom =""
   console.log(counter)
-  if(counter == 1){
-    button4.style.paddingTop = '200px'
-    button4.style.top = "25%"
-    button4.style.left = "47%"
-  }
-  if(counter == 2){
-    button4.style.paddingBottom = '100px'
-    button4.style.bottom = "10%"
-    button4.style.right = "2%"
-  }
+  if (counter == 1) {
+  button4.style.paddingTop = '4vw';
+  button4.style.top = '37%';
+  button4.style.left = '47%';
+}
+
+if (counter == 2) {
+  button4.style.paddingBottom = '4vw';
+  button4.style.top = '78%';
+  button4.style.left = '90%';
+}
   if(counter == 3){
     button4.style.display = 'none'
     button5.style.display = 'block'
   }
   if(counter == 4){
-    button4.style.paddingRight = '100px'
-    button4.style.bottom = "20%"
-    button4.style.left = "6%"
+    button4.style.paddingRight = '4vw';
+    button4.style.top = '76%';
+    button4.style.left = '5%';
   }
   if(counter == 5){
-    button4.style.paddingBottom = '50px'
-    button4.style.bottom = "11%"
-    button4.style.left = "0"
+    button4.style.paddingBottom = '4vw'
+    button4.style.top = '81%';
+    button4.style.left = '0';
   }
   if(counter == 6){
-    button4.style.paddingRight = '100px'
-    button4.style.bottom = "18%"
-    button4.style.left = "6%"
+    button4.style.paddingRight = '8vw'
+     button4.style.top = '77%';
+    button4.style.left = '6%';
   }
   if(counter == 7){
-    button4.style.paddingTop = '50px'
-    button4.style.top = "25%"
-    button4.style.right = "-2%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "22%"
+    button4.style.left = '91%';
   }
   if(counter == 8){
-    button4.style.paddingTop = '100px'
-    button4.style.top = "21%"
-    button4.style.right = "-3%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "19%"
+    button4.style.left = '91%';
   }
   if(counter == 9){
     button4.style.display = 'none'
@@ -67,14 +86,14 @@ changeButton = () => {
     button5.style.display = 'block'
   }
   if(counter == 11){
-    button4.style.paddingBottom = '100px'
+    button4.style.paddingBottom = '4vw'
     button4.style.top = "42%"
-    button4.style.right = "25%"
+    button4.style.left = '63%';
   }
   if(counter == 12){
-    button4.style.paddingTop = '100px'
-    button4.style.top = "20%"
-    button4.style.right = "-3%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "22%"
+    button4.style.left = '92%';
   }
   if(counter == 13){
     button4.style.display = 'none'
@@ -93,32 +112,32 @@ changeButton = () => {
     button5.style.display = 'block'
   }
   if(counter == 17){
-    button4.style.paddingBottom = '100px'
-    button4.style.bottom = "13%"
-    button4.style.right = "-1%"
+    button4.style.paddingBottom = '4vw'
+    button4.style.top = "73%"
+    button4.style.left = '89%';
   }
   if(counter == 18){
     button4.style.paddingBottom = '50px'
-    button4.style.bottom = "11%"
-    button4.style.right = "75%"
+    button4.style.top = "81%"
+    button4.style.left = '13%';
   }
   if(counter == 19){
-    button4.style.paddingTop = '100px'
-    button4.style.top = "8%"
-    button4.style.right = "8%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "10%"
+    button4.style.left = '81%';
   }
   if(counter == 20){
-    button4.style.paddingTop = '100px'
-    button4.style.top = "13%"
-    button4.style.right = "8%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "14%"
+    button4.style.left = '81%'
   }
   if(counter == 21){
     button4.style.display = 'none'
     button5.style.display = 'block'
   }
   if(counter == 22){
-    button4.style.paddingTop = '100px'
-    button4.style.bottom = "22%"
+    button4.style.paddingTop = '4vw'
+    button4.style.top = "66%"
     button4.style.left = "13%"
   }
   if(counter == 23){
@@ -126,9 +145,9 @@ changeButton = () => {
     button5.style.display = 'block'
   }
   if(counter == 24){
-    button4.style.paddingBottom = '100px'
+    button4.style.paddingBottom = '4vw'
     button4.style.top = "60%"
-    button4.style.right = "25%"
+    button4.style.left = "63%"
   }
   if(counter == 25){
     button4.style.display = 'none'
@@ -189,23 +208,7 @@ function doSomeThing() {
   hideButtons()
   video.src = `./public/new/${counter}.mp4`;
 }
-// On Video End
-myFunction = () =>  {
-  button2.style.display = 'block'
-  button4.style.display = "block"
-  if(counter > 1){
-    button3.style.display = "block"
-  }
-  button4.className = "animate__animated animate__pulse animate__infinite	infinite"
-  button4.style.paddingTop = ''
-  button4.style.paddingBottom = ''
-  button4.style.paddingLeft = ''
-  button4.style.top =""
-  button4.style.right =""
-  button4.style.left =""
-  button4.style.bottom =""
-  changeButton()
-}
+
 // For Video Reply
 function goReplay() {
   counter
